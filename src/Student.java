4b3bed3cadcb5e148b1f3db5296dadfcacdae56d
Tosin <AKINMOLADUNO0214@students.bowie.edu>
2026-09-03 10:46:47 -0400

    OC - 4(CC - SG)

--- src/Student.java
+++ src/Student.java
@@ -64,6 +64,12 @@ public class Student {
     // You can create overloaded constructors by 1)varying the parameter number, 2) order
     public static void main(String[] args) {
 
+         Student s6 = new Student();
+         // Setting names
+         s6.FN = "Melanie";
+         s6.setLN("Thomas");
+         s6.LN = "Simpson";
+
         Student s4 = new Student("Helena","Payton");
         System.out.println(s4.FN);
         System.out.println(s4.LN);
