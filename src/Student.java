9cfe9987b1c2d90045e5c311c5d8ae85479f4991
Tosin <AKINMOLADUNO0214@students.bowie.edu>
2026-09-03 09:58:48 -0400

    OC - 2

--- src/Student.java
+++ src/Student.java
@@ -25,5 +25,19 @@ public class Student {
         s1 = new Student();
         System.out.println(s1);
         System.out.println(s1.FN);
+
+        Student s2;
+        s2 = new Student("Tobiloba", "Ayodeji", 14141);
+        System.out.println(s2.FN);
+        System.out.println(s2.LN);
+
+
+        Student s3;
+        String FN = "Miguel";
+        String LN = "Gasacaortega";
+        int Sid = 31415;
+        s3 = new Student(FN, LN, Sid);
+        System.out.println(s3.FN);
+        System.out.println(s3.LN);
     }
 }
